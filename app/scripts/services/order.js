f8f800e72e9e10bf8aee2362ea2674f3d64f06a3
Josh.Hilland <jdhilland@gmail.com>
2016-10-11 13:37:21 +0100

    Re-routed the orders API calls to use the patient ID provided by the Mater TIE, and removed the create button

--- app/scripts/services/order.js
+++ app/scripts/services/order.js
@@ -4,30 +4,18 @@ angular.module('mcr-ui')
   .factory('Order', function ($http) {
 
     var all = function (patientId) {
-      // TODO reinstate the following line when test data for Mater patients has been loaded
-      // return $http.get('/api/patients/' + patientId + '/laborders');
-
-      return $http.get('/api/patients/' + '9999999000' + '/laborders');
+      return $http.get('/api/patients/' + patientId + '/laborders');
     };
 
     var get = function (patientId, compositionId, source) {
-      // TODO reinstate the following line when test data for Mater patients has been loaded
-      // return $http.get('/api/patients/' + patientId + '/laborders/' + compositionId + '?source=' + source);
-
-      return $http.get('/api/patients/' + '9999999000' + '/laborders/' + compositionId + '?source=' + source);
+      return $http.get('/api/patients/' + patientId + '/laborders/' + compositionId + '?source=' + source);
     };
 
     var create = function (patientId, composition) {
-      // TODO reinstate the following line when test data for Mater patients has been loaded
-      // return $http.post('/api/patients/' + patientId + '/laborders', composition);
-
-      return $http.post('/api/patients/' + '9999999000' + '/laborders', composition);
+      return $http.post('/api/patients/' + patientId + '/laborders', composition);
     };
 
     var suggestion = function () {
-      // TODO reinstate the following line when test data for Mater patients has been loaded
-      // return $http.get('/api/terminology/list/order');
-
       return $http.get('/api/terminology/list/order');
     };
 
