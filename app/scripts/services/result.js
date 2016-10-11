ca7a6ca69a61006e732a9b9fad6b3736ab8b336b
Josh.Hilland <jdhilland@gmail.com>
2016-10-11 16:58:08 +0100

    Re-routed the results API calls to use the patient ID provided by the Mater TIE, and removed the create button

--- app/scripts/services/result.js
+++ app/scripts/services/result.js
@@ -4,17 +4,11 @@ angular.module('mcr-ui')
   .factory('Result', function ($http) {
 
     var get = function (patientId, compositionId, source) {
-      // TODO reinstate the following line when test data for Mater patients has been loaded
-      // return $http.get('/api/patients/' + patientId + '/labresults/' + compositionId + '?source=' + source);
-
-      return $http.get('/api/patients/' + '9999999000' + '/labresults/' + compositionId + '?source=' + source);
+      return $http.get('/api/patients/' + patientId + '/labresults/' + compositionId + '?source=' + source);
     };
 
     var all = function (patientId) {
-      // TODO reinstate the following line when test data for Mater patients has been loaded
-      // return $http.get('/api/patients/' + patientId + '/labresults');
-
-      return $http.get('/api/patients/' + '9999999000' + '/labresults');
+      return $http.get('/api/patients/' + patientId + '/labresults');
     };
 
     return {
